59dfa178dde07121b3fa179753b91ff2b4dba28c
Angel Vazurio <angelCL0@outlook.com>
2022-02-28 14:26:02 -0500

    fixed

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,13 +1,19 @@
 import styles from "./header.module.css";
 import cn from "classnames";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <section className={cn("section-header")}>
       <div className={cn("container", styles.container)}>
-        <p className={cn("body-bold logo", styles.logo)}>
-          Mis <span>heros</span>
-        </p>
+        <Link href='/'>
+          <a>
+            <p className={cn("body-bold logo", styles.logo)}>
+              Mis <span>heros</span>
+            </p>
+          </a>
+        </Link>
+
         <button className={cn("button")}>👋</button>
       </div>
     </section>

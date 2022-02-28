02636301ed02bc7cfb342fc59f1cf6adc477cd86
Angel Vazurio <angelCL0@outlook.com>
2022-02-27 19:55:39 -0500

    sle

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import useSWR from "swr";
-
-const fetcher = (...args) => fetch(...args).then((response) => response.json());
-
 export default function handler(req, res) {
-  const { data, error } = useSWR("/api/user/123", fetcher);
   res.status(200).json({ name: "John Doe" });
 }

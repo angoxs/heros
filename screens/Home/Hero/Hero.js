8310a5a45aecc1f8763d78024aa2a5be95d8307a
Angel Vazurio <angelCL0@outlook.com>
2022-03-01 18:47:07 -0500

    fixed

--- screens/Home/Hero/Hero.js
+++ screens/Home/Hero/Hero.js
@@ -1,18 +1,33 @@
 import styles from "./hero.module.css";
 import cn from "classnames";
+import { motion } from "framer-motion";
 
 export default function Hero({ userList }) {
   return (
     <div className={cn("section section-pb")}>
       <div className={cn("container")}>
         <div className={styles.content}>
-          <h1 className={cn("h1", styles.title)}>
-            Where fans meet their <span>heros.</span>
-          </h1>
-          <p className={cn("body", styles.subtitle)}>
-            A place where you can meet and interact with your idols and mentors
-            in a deeper way.
-          </p>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.2 }}
+            viewport={{ once: true }}
+          >
+            <h1 className={cn("h1", styles.title)}>
+              Where fans meet their <span>heros.</span>
+            </h1>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4 }}
+            viewport={{ once: true }}
+          >
+            <p className={cn("body", styles.subtitle)}>
+              A place where you can meet and interact with your idols and
+              mentors in a deeper way.
+            </p>
+          </motion.div>
         </div>
       </div>
     </div>

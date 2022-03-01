35148fac8f85512d1688b9a2400cd5f8643f52bc
Angel Vazurio <angelCL0@outlook.com>
2022-03-01 16:31:27 -0500

    modal button

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -7,15 +7,17 @@ export default function Modal({ closeModal, setModal, modal }) {
   return (
     <OutsideClickHandler onOutsideClick={() => setModal(false)}>
       <div modal={modal} className={styles.content}>
-        <button className={styles.close_button} onClick={closeModal}>
+        <button
+          className={cn("button", styles.close_button)}
+          onClick={closeModal}
+        >
           <svg
-            width='24'
-            height='24'
+            width='44'
+            height='44'
             viewBox='0 0 24 24'
             fill='none'
             xmlns='http://www.w3.org/2000/svg'
           >
-            <path d='M3.35288 8.95043C4.00437 6.17301 6.17301 4.00437 8.95043 3.35288C10.9563 2.88237 13.0437 2.88237 15.0496 3.35288C17.827 4.00437 19.9956 6.17301 20.6471 8.95044C21.1176 10.9563 21.1176 13.0437 20.6471 15.0496C19.9956 17.827 17.827 19.9956 15.0496 20.6471C13.0437 21.1176 10.9563 21.1176 8.95044 20.6471C6.17301 19.9956 4.00437 17.827 3.35288 15.0496C2.88237 13.0437 2.88237 10.9563 3.35288 8.95043Z' />
             <path
               d='M13.7678 10.2322L10.2322 13.7677M13.7678 13.7677L10.2322 10.2322'
               strokeLinecap='round'

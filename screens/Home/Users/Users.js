f70abd00adce0aa1b6765c1d1e4af191e6542ba9
Angel Vazurio <angelCL0@outlook.com>
2022-03-02 00:03:27 -0500

    search bar

--- screens/Home/Users/Users.js
+++ screens/Home/Users/Users.js
@@ -2,12 +2,36 @@ import cn from "classnames";
 import styles from "./users.module.css";
 import User from "../../../components/User";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 export default function Users({ users }) {
+  const [inputText, setInputText] = useState("");
+  let inputHandler = (event) => {
+    var lowerCase = event.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
+  const filteredData = users.filter((element) => {
+    if (inputText === "") {
+      return element;
+    } else {
+      return element.firstName.toLowerCase().includes(inputText);
+    }
+  });
+
   return (
     <div className={cn("section section-pt", styles.section)}>
       <div className={styles.users_wrapper}>
-        {users.map((user) => (
+        <div className={styles.label}>
+          <input
+            className={cn("caption-bold", styles.input)}
+            onChange={inputHandler}
+            placeholder='Search Users'
+            type='text'
+          />
+        </div>
+
+        {filteredData.map((user) => (
           <motion.div
             initial={{ opacity: 0, y: 30 }}
             whileInView={{ opacity: 1, y: 0 }}
